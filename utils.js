e6e3ed38c75a4b0e9df297793ec7ddee7fa51377
Janier Rodríguez López <janierbits@yahoo.com>
2023-08-12 02:31:30 -0400

    add new  readJsonFile util

--- utils.js
+++ utils.js
@@ -26,6 +26,23 @@ function deleteDir(dirPath) {
     });
 }
 
+async function readJsonFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            try {
+                const jsonData = JSON.parse(data);
+                resolve(jsonData);
+            } catch (parseError) {
+                reject(parseError);
+            }
+        });
+    });
+}
+
 module.exports = {
-    deleteDir
+    deleteDir,
+    readJsonFile
 }
